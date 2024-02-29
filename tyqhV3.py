1a7b0859ce381d02a87ca0ab68a30af742085dba
Code.K <160496306+Code-KKK@users.noreply.github.com>
2024-02-29 16:11:24 +0800

    更新文件

--- tyqhV3.py
+++ tyqhV3.py
@@ -43,20 +43,22 @@ file_url = 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/Code-KK
 def check_environment(file_name):
     v, o, a = sys.version_info, platform.system(), platform.machine()
     print(f"Python版本: {v.major}.{v.minor}.{v.micro}, 操作系统类型: {o}, 处理器架构: {a}")
-    if (v.minor in [10,11]) and o.lower() in ['linux'] and a.lower() in ['x86_64']:
+    if (v.minor in [8,9,10,11]) and o.lower() in ['linux'] and a.lower() in ['x86_64','aarch64']:
         print("当前环境符合运行要求")
         if o.lower() == 'linux':
             file_name += '.so'
             main_run(file_name, v.minor, o.lower(), a.lower())
     else:
-        if not (v.minor in [10,11]):
-            print("不符合运行要求: Python版本不是3.10.X 或 3.11.X")
+        if not (v.minor in [8,9,10,11]):
+            print("不符合运行要求: Python版本不是 3.8 ~ 3.11")
         if not (o.lower() in ['linux']):
             print(f"不符合运行要求: 操作系统类型[{o}] 支持：Linux")
         if not (a.lower() in ['x86_64','aarch64']):
-            print(f"不符合运行要求: 当前处理器架构[{a}] 仅支持：x86_64")
+            print(f"不符合运行要求: 当前处理器架构[{a}] 仅支持：x86_64,aarch64")
 
 def main_run(file_name, py_v, os_info, cpu_info):
+    if cpu_info == 'aarch64':
+        print('当前aarch64架构，如遇青龙容器运行报错，请先在青龙-依赖管理-Linux，添加gcc-aarch64-linux-gnu')
     if os.path.exists(file_name):
         file_name_ = os.path.splitext(file_name)[0]
         Code_module = __import__(file_name_)
@@ -67,7 +69,7 @@ def main_run(file_name, py_v, os_info, cpu_info):
                 executor.submit(run.main)
                 time.sleep(random.randint(2, 3))
     else:
-        print(f"不存在{file_name}依赖模块,准备下载模块文件")
+        print(f"不存在{file_name}功能模块,准备下载模块文件")
         download_file(file_name, py_v, os_info, cpu_info,file_url)
 
 def download_file(file_name, py_v, os_info, cpu_info, url):
@@ -78,11 +80,17 @@ def download_file(file_name, py_v, os_info, cpu_info, url):
         url = url + f'{file_name_}/{file_name_}.cp3{py_v}-win_{cpu_info}.pyd'
     try:
         print(url)
-        subprocess.run(["curl",'-#',"-o", file_name, url], check=True)
-        print(f"{file_name}文件下载成功~开始执行~")
-        check_environment(file_name_)
+        result = subprocess.run(['curl', '-I', '-s', '-o', '/dev/null', '-w', '%{http_code}', url], capture_output=True, text=True)
+        if result.stdout.strip() == '404':
+            print('服务器文件不存在,已停止下载')
+        else:
+            print('服务器文件存在，将开始下载')
+            subprocess.run(["curl",'-#',"-o", file_name, url], check=True)
+            print(f"{file_name}文件下载成功~开始执行~")
+            check_environment(file_name_)
     except subprocess.CalledProcessError:
         print("下载失败，请检查 URL 或 网络问题。")
+
 
 if __name__ == '__main__':
     print = partial(print, flush=True)
